53659c2a1277d816766223ac35cd3ef449acded8
bhusari183 <96298449+bhusari183@users.noreply.github.com>
2021-12-21 10:01:42 +0530

    shubham_git
    by shubham

--- GitTB12/src/com/cjc/Demo.java
+++ GitTB12/src/com/cjc/Demo.java
@@ -12,4 +12,10 @@ System.out.println("m1");
 System.out.println("m2");
 	System.out.println("m3");	
 }
+public void m2()
+{
+System.out.println("m1");	
+System.out.println("m2");
+	System.out.println("m3");	
+}	
 }
