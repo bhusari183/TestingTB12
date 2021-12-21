6fed841aa126d27c21ab8e13c4ecf196d48ca93a
bhusari183 <96298449+bhusari183@users.noreply.github.com>
2021-12-21 20:40:50 +0530

    Update Demo.java

--- GitTB12/src/com/cjc/Demo.java
+++ GitTB12/src/com/cjc/Demo.java
@@ -18,12 +18,7 @@ System.out.println("m1");
 System.out.println("m2");
 	System.out.println("m3");	
 }
-public void m21()
-{
-System.out.println("m1");	
-System.out.println("m2");
-	System.out.println("m3");	
-}	
+
 	public void m23()
 {
 System.out.println("m1");	
